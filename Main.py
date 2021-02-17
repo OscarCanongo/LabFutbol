537e648bfa10c6afca7c312363d98178dac3312e
OscarCanongo <oscarcanongo@outlook.com>
2021-02-17 13:46:54 -0600

    Leyendo csvs

---
+++ Main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+defensivos = pd.read_csv("Defensivos.csv")
+print(defensivos)
+
+distribucion = pd.read_csv("Distribucion.csv")
+print(distribucion)
+
+ofensivos = pd.read_csv("Ataque.csv")
+print(ofensivos)
