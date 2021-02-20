805417fc4169203c8d11fb1e3e0729eb6cf9026c
Benny Ruiz Jimenez <A01328177@itesm.mx>
2021-02-19 22:57:44 -0600

    Correcion programa PIVOT

--- pivotDataset.py
+++ pivotDataset.py
@@ -1,9 +1,10 @@
 import pandas as pd
 file = "/Users/benny/Documents/LabFutbol/Laterales.csv"
 df = pd.read_csv(file)
-print(df.head())
+# print(df.head())
+print(df.dtypes)
+df_T = df.T
+print(df.dtypes)
 
-df = df.pivot_table(columns="Nombre")
 print(df.head())
-
-df.to_csv("pivotTabla.csv")
+df_T.to_csv("pivotTabla.csv")
